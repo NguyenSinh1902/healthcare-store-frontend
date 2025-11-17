349f39c0326e29d228d544c99bf5c7e0b611c468
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-11-17 17:21:26 +0700

    feat(footer): add homepage footer UI

--- src/components/DefaultComponent/DefaultComponent.jsx
+++ src/components/DefaultComponent/DefaultComponent.jsx
@@ -1,10 +1,18 @@
+import React from 'react';
 import Header from '../Header/Header';
+// 1. Import component Footer (Giả sử nằm cùng cấp hoặc đúng đường dẫn)
+import Footer from '../Footer/Footer'; 
 
 const DefaultComponent = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+            
+      <main>
+        {children}
+      </main>
+      
+      <Footer />
     </>
   );
 };
