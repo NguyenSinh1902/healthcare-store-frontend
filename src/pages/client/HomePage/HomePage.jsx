57ba0a0c11aaa2322824de89fae8bd51ec33b9bf
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-11-19 19:15:23 +0700

    feat(homepage): add homepage Recommended Products UI

--- src/pages/client/HomePage/HomePage.jsx
+++ src/pages/client/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../../../components/Banner/Banner';
 import FlashSales from '../../../components/FlashSales/FlashSales';
 import BestSellingProducts from '../../../components/BestSellingProducts/BestSellingProducts';
-
+import RecommendedProducts from '../../../components/RecommendedProducts/RecommendedProducts'
 
 const HomePage = () => {
   return (
@@ -11,6 +11,7 @@ const HomePage = () => {
       <FlashSales />
       
         <BestSellingProducts/>
+        <RecommendedProducts/>
       
       
     </div>
