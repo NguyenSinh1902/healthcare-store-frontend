ab198b65bc3ba40d61a93e574ceb5c1f160a9c17
mongtho <mongthodang07032002@gmail.com>
2025-11-23 01:03:09 +0700

    feat(checkout): update routes.js(add CategoryPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import CategoryPage from "../pages/client/Category/Category";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
+
+   { path: "/category", element: <CategoryPage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 ];
