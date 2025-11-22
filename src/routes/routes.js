dd7169d30a3bcdee19770a723755eefba9f06e8c
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-11-22 21:05:30 +0700

    feat(cart): update routes.js(add CartPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import CartPage from "../pages/client/Cart/CartPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
+
+  { path: "/cart", element: <CartPage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 ];
