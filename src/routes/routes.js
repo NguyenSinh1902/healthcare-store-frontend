5b434439d0bed61611eb1540e9b8080aa1a8fcdc
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-11-22 22:50:43 +0700

    feat(checkout): update routes.js(add OrderSuccessPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,14 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
 import OrderPage from "../pages/client/Order/OrderPage";
+import OrderSuccessPage from "../pages/client/OrderSuccess/OrderSuccessPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/order", element: <OrderPage />, isShowHeader: true },
+
+  { path: "/order-success", element: <OrderSuccessPage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 ];
