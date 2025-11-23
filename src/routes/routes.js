51c9ef0b0898829880ff247542bc57739577f92f
MihnThihn <pvmthinh2002@gmail.com>
2025-11-23 21:18:48 +0700

    feat(auth-fe): update routes.js(add LoginPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import LoginPage from "../pages/client/Login/LoginPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
+
+  { 
+    path: "/login", element: <LoginPage />, isShowHeader: false
+  },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 ];
