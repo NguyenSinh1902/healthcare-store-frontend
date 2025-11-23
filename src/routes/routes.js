6952ecfad523046deb18d4305bffed3df9286bc8
MihnThihn <pvmthinh2002@gmail.com>
2025-11-23 21:19:55 +0700

    feat(auth-fe): update routes.js(add RegisterPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
 import LoginPage from "../pages/client/Login/LoginPage";
+import RegisterPage from "../pages/client/Register/RegisterPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { 
     path: "/login", element: <LoginPage />, isShowHeader: false
+  },
+
+  { 
+    path: "/register", element: <RegisterPage />, isShowHeader: false
   },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
