e038b8293a76108587a03925275f287f8b0da6b7
mongtho <mongthodang07032002@gmail.com>
2025-11-23 18:54:31 +0700

    feat(product): update routes.js(add AccountDetailsPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import AccountDetailsPage from "../pages/client/Account/AccountDetails/AccountDetailsPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
+
+  { path: "/account/details", element: <AccountDetailsPage />, isShowHeader: true },
+
+  { path: "/account", element: <AccountDetailsPage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 ];
