3308c1a7bf69681bc890766cadbd45331bc2d590
MihnThihn <pvmthinh2002@gmail.com>
2025-11-23 19:23:20 +0700

    feat(orderTracking): update routes.js(add MyOrdersPage)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,14 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import MyOrdersPage from "../pages/client/Account/MyOrders/MyOrdersPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
+
+  { 
+    path: "/account/orders", element: <MyOrdersPage />, isShowHeader: true 
+  },
+
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 ];
