36bfa30b5f060f3dd1f77bb66d3fc0e02ccd590b
Rekddy <phithien1007@gmail.com>
2025-11-30 19:14:58 +0700

    refactor(admin-dashboard): Move Dashboard component to components/AdminComponent directory

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
-import Dashboard from "../pages/admin/Dashboard/Dashboard";
+import DashboardPage from "../pages/admin/Dashboard/DashboardPage";
 import HomePage from "../pages/client/HomePage/HomePage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
-  { path: "/admin", element: <Dashboard />, isShowHeader: false },
+  { path: "/admin", element: <DashboardPage />, isShowHeader: false },
 ];
 
 export default routes;
