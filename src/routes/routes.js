d085521281a4f5b98759582b095c457fa419a7cc
Rekddy <phithien1007@gmail.com>
2025-11-30 18:51:35 +0700

    feat(routing): Add admin login and register routes to the application

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,17 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
 
+import AdminLogin from "../pages/admin/Login/LoginPage";
+import AdminRegister from "../pages/admin/Register/RegisterPage";
+
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
+
+  { path: "/admin/login", element: <AdminLogin />, isShowHeader: false },
+  
+  { path: "/admin/register", element: <AdminRegister />, isShowHeader: false },
 ];
 
 export default routes;
