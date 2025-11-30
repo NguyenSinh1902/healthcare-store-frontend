4f43abdf3eb7d838187eed52edff953508a5487e
MihnThihn <pvmthinh2002@gmail.com>
2025-12-01 00:40:43 +0700

    feat(routing): Add route for Admin Staff Management page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
 import AdminUserPage from "../pages/admin/User/UserPage";
+import StaffPage from "../pages/admin/Staff/StaffPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
@@ -8,6 +9,8 @@ const routes = [
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
 
   { path: "/admin/customers", element: <AdminUserPage />, isShowHeader: false },
+
+  { path: "/admin/staff", element: <StaffPage />, isShowHeader: false },
 ];
 
 export default routes;
