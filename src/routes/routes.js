942059a5380dd288b4ed1303265f12862a40373f
MihnThihn <pvmthinh2002@gmail.com>
2025-11-30 21:51:22 +0700

    feat(routing): Add route for Admin Product Management page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import ProductPage from "../pages/admin/Product/ProductPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
+
+  { path: "/admin/products", element: <ProductPage />, isShowHeader: false },
 ];
 
 export default routes;
