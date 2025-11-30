a947cfb45299ed7f173c2896a9e35dedc12aa8a2
Rekddy <phithien1007@gmail.com>
2025-11-30 19:46:31 +0700

    feat(routing): Add route for Admin Order Management page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import AdminOrderPage from "../pages/admin/Order/OrderPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
+
+    { path: "/admin/orders", element: <AdminOrderPage />, isShowHeader: false },
 ];
 
 export default routes;
