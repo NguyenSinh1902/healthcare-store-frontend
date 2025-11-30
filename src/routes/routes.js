0062636b0714065fed50bb99b9f4583d5b7910f1
MihnThihn <pvmthinh2002@gmail.com>
2025-11-30 22:05:03 +0700

    feat(routing): Add route for Admin Category Management page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import AdminCategoryPage from "../pages/admin/Category/CategoryPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
+
+  { path: "/admin/categories", element: <AdminCategoryPage />, isShowHeader: false },
 ];
 
 export default routes;
