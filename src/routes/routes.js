99f745234b95df250e587fc2251522fc635e1dc1
MihnThihn <pvmthinh2002@gmail.com>
2025-12-01 00:28:54 +0700

    feat(routing): Add AdminUserPage route file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import AdminUserPage from "../pages/admin/User/UserPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
+
+  { path: "/admin/customers", element: <AdminUserPage />, isShowHeader: false },
 ];
 
 export default routes;
