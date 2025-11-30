af9fd5a872b93f8983177996411e70ed5b4f9286
Rekddy <phithien1007@gmail.com>
2025-11-30 21:10:52 +0700

    feat(routing): Add route for Admin Coupon Management page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import Dashboard from "../pages/admin/Dashboard/Dashboard";
 import HomePage from "../pages/client/HomePage/HomePage";
+import CouponPage from "../pages/admin/Coupon/CouponPage";
 
 const routes = [
   { path: "/", element: <HomePage />, isShowHeader: true },
 
   { path: "/admin", element: <Dashboard />, isShowHeader: false },
+
+  { path: "/admin/coupons", element: <CouponPage />, isShowHeader: false },
 ];
 
 export default routes;
