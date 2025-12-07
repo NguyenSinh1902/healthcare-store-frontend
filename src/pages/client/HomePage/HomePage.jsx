3232fbd138b8767b7004733ca31f052e3f15555b
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-12-07 22:42:30 +0700

    feat(homepage): finalize layout and assemble components in HomePage

--- src/pages/client/HomePage/HomePage.jsx
+++ src/pages/client/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../../../components/Banner/Banner";
 import FlashSales from "../../../components/FlashSales/FlashSales";
 import BestSellingProducts from "../../../components/BestSellingProducts/BestSellingProducts";
@@ -6,24 +6,68 @@ import RecommendedProducts from "../../../components/RecommendedProducts/Recomme
 import FeaturedProduct from "../../../components/FeaturedProduct/FeaturedProduct";
 import NewArrival from "../../../components/NewArrival/NewArrival";
 import AboutUs from "../../../components/AboutUs/AboutUs";
+import { getProductsByGroup } from "../../../services/productService";
 
 const HomePage = () => {
+  const [flashSaleProducts, setFlashSaleProducts] = useState([]);
+  const [bestSellingProducts, setBestSellingProducts] = useState([]);
+  const [recommendedProducts, setRecommendedProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const [flashSaleRes, bestSellingRes, recommendedRes] = await Promise.all([
+          getProductsByGroup("FLASH_SALE"),
+          getProductsByGroup("BEST_SELLING"),
+          getProductsByGroup("RECOMMENDED"),
+        ]);
+
+        if (flashSaleRes.success) {
+          setFlashSaleProducts(mapProductData(flashSaleRes.data));
+        }
+        if (bestSellingRes.success) {
+          setBestSellingProducts(mapProductData(bestSellingRes.data));
+        }
+        if (recommendedRes.success) {
+          setRecommendedProducts(mapProductData(recommendedRes.data));
+        }
+
+      } catch (error) {
+        console.error("Error fetching homepage products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const mapProductData = (data) => {
+    return data.map((item) => ({
+      id: item.idProduct,
+      name: item.nameProduct,
+      price: item.price,
+      originalPrice: item.oldPrice,
+      discount: item.discountPercent ? `-${item.discountPercent}%` : null,
+      rating: 5, // API doesn't provide rating yet(fake data)
+      image: item.imageProduct,
+    }));
+  };
+
   return (
     <div>
       <Banner />
 
-      <FlashSales />
+      <FlashSales products={flashSaleProducts} />
 
-      <BestSellingProducts />
+      <BestSellingProducts products={bestSellingProducts} />
 
       <FeaturedProduct />
 
-      <RecommendedProducts />
+      <RecommendedProducts products={recommendedProducts} />
 
       <NewArrival />
 
       <AboutUs />
-    </div>
+    </div >
   );
 };
 
