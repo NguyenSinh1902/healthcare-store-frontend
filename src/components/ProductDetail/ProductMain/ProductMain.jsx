cc265d1ca442f06c078232a01025f2e07a0f48da
mongtho <mongthodang07032002@gmail.com>
2025-12-08 11:45:44 +0700

    feat(components): update ProductMain UI and interaction logic

--- src/components/ProductDetail/ProductMain/ProductMain.jsx
+++ src/components/ProductDetail/ProductMain/ProductMain.jsx
@@ -3,19 +3,20 @@ import { Row, Col } from 'antd';
 import ProductImage from './ProductImage';
 import ProductInfo from './ProductInfo';
 
-const ProductMain = () => {
+const ProductMain = ({ product }) => {
+  const images = product && product.thumbnails && product.thumbnails.length > 0
+    ? product.thumbnails
+    : ['https://placehold.co/500x600?text=No+Image'];
+
   return (
     <div className="product-main-wrapper" style={{ marginBottom: '80px' }}>
       <Row gutter={150}>
-
         <Col xs={24} lg={14} xl={14}>
-           <ProductImage />
+          <ProductImage images={images} />
         </Col>
-
         <Col xs={24} lg={10} xl={10}>
-           <ProductInfo />
+          <ProductInfo product={product} />
         </Col>
-        
       </Row>
     </div>
   );
