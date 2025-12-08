963eb3f1cfb5bad0723b3263f278f6ad30721fed
Rekddy <phithien1007@gmail.com>
2025-12-08 14:28:38 +0700

    feat(services): add authService for admin and update api config

---
+++ src/services/authService.js
@@ -0,0 +1,17 @@
+import api from './api'; // Đảm bảo import đúng file cấu hình axios của bạn
+
+export const register = (fullName, email, password, confirmPassword, role) => {
+    // KHÔNG dùng async/await ở đây, return thẳng Promise của axios
+    // Để Component tự xử lý .data
+    return api.post('/auth/register', {
+        fullName,
+        email,
+        password,
+        confirmPassword,
+        role
+    });
+};
+
+export const login = (email, password) => {
+    return api.post('/auth/login', { email, password });
+};
