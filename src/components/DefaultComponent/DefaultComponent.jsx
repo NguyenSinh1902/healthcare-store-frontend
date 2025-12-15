74103209324e8191be008466f41edfc8679887f1
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-12-15 18:02:18 +0700

    feat(chatbot): add <Chatbox /> in DefaultComponent

--- src/components/DefaultComponent/DefaultComponent.jsx
+++ src/components/DefaultComponent/DefaultComponent.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Header from '../Header/Header';
-// 1. Import component Footer (Giả sử nằm cùng cấp hoặc đúng đường dẫn)
-import Footer from '../Footer/Footer'; 
+import Chatbox from '../Chatbox/Chatbox';
+import Footer from '../Footer/Footer';
 
 const DefaultComponent = ({ children }) => {
   return (
     <>
       <Header />
-            
+
       <main>
         {children}
       </main>
-      
+
+      <Chatbox />
       <Footer />
     </>
   );
